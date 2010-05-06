d055de268a1fa2051caae6e7518dd7fcad2bdeda
erik <erikeldridge@yahoo.com>
2010-05-06 00:03:45 -0700

    added comments describing openid hop pattern

--- op.spring.php
+++ op.spring.php
@@ -19,18 +19,22 @@ function yql( $query )
     return $response;
 }
 
+//1) pass in openid & app identifier
 $openid = filter_var( $_GET['openid'], FILTER_SANITIZE_STRING );
 $appid = filter_var( $_GET['appid'], FILTER_SANITIZE_STRING );
 
-//look up return to
+//2) look up return to based on app id
 $return_to = 'http://example.com/';
 
+//3) discover openid login url
 $query = sprintf( "use '%s' as openid; select * from openid where id='%s' and return_to='%s'",
     'http://gist.github.com/yql/yql-tables/raw/master/openid/openid.xml',
 	$openid,
 	$return_to
 );
 $response = yql( $query );
+
+//4) redirect user to log in
 if ( $response && $response->query->results->success ) {
     header( "Location: ".$response->query->results->success );
 }
